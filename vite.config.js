74ef46b7cb196465fb08cf8c7351f2b12fd0070f
R Chula <r0bert.chula@gmail.com>
2026-09-15 09:34:21 -0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,46 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        preserveSymlinks: true,
+    },
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        strictPort: true,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-
         vue(),
     ],
 });
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+
+//         vue(),
+//     ],
+// });
+
+// export default defineConfig({
+//     server: {
+//         host: '127.0.0.1',
+//         port: 5173,
+//         strictPort: true,
+//     },
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         vue(),
+//     ],
+// });
+
